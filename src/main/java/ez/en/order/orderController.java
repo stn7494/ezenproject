778fb478bc5175ae1518cb5d81555f4ea318c563
SHIN\stn7494 <stn7494@gmail.com>
2023-09-13 10:29:16 +0900

    초기버전

---
+++ src/main/java/ez/en/order/orderController.java
@@ -0,0 +1,14 @@
+package ez.en.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class orderController {
+
+    @GetMapping("/order/index")
+    public void index(){
+
+    }
+
+}
---
+++ src/main/java/ez/en/procurement/procurementController.java
@@ -0,0 +1,13 @@
+package ez.en.procurement;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class procurementController {
+
+    @GetMapping("/procurement/index")
+    public void index(){
+
+    }
+}
