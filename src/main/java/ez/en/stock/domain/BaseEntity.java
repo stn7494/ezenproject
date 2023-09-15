5752e58e8acb676bf5ed26e77ac7c2d58e60db57
SHIN\stn7494 <stn7494@gmail.com>
2023-09-15 10:48:04 +0900

    초기세팅

---
+++ src/main/java/ez/en/stock/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package ez.en.stock.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, name = "dsdate")
+    private LocalDateTime pDate;
+
+
+
+}
