6fceefb82f89bc1b1d8a02969eb0f5d3d1840aa9
KHY <mmuroy@gmail.com>
2023-09-15 14:48:31 +0900

    a

--- src/main/java/ez/en/order/repository/ProgressinspectionRepository.java
+++
@@ -1,7 +0,0 @@
-package ez.en.order.repository;
-
-import ez.en.order.domain.Progressinspection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProgressinspectionRepository extends JpaRepository<Progressinspection, Integer> {
-}
