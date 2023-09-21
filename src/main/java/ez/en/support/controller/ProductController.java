d59b8e54e15378cd599ebc758b105235fdee92c7
kdj <argendadong@gmail.com>
2023-09-21 17:36:43 +0900

    0921 품목분류선택+품목코드자동생성+유효성검사

--- src/main/java/ez/en/support/controller/ProductController.java
+++ src/main/java/ez/en/support/controller/ProductController.java
@@ -21,7 +21,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.Arrays;
 
 @Controller
-@RequestMapping("/support")
+@RequestMapping("/product")
 @Log4j2
 @RequiredArgsConstructor
 public class ProductController {
@@ -29,7 +29,7 @@ public class ProductController {
     private final ProductService productService;
 
     //품목전체목록 보여주기
-    @GetMapping("/productList")
+    @GetMapping("/list")
     public void list(ProductPageRequestDTO productPageRequestDTO, Model model){ // list메소드로 페이징처리와 검색에 이용
         ProductPageResponseDTO<ProductDTO> responseDTO = productService.list(productPageRequestDTO);
         log.info(responseDTO);
@@ -37,12 +37,12 @@ public class ProductController {
     }
 
     //품목등록
-    @GetMapping("/productRegister")
+    @GetMapping("/register")
     public void productregisterGET() {
 
     }
 
-    @PostMapping("/productRegister")
+    @PostMapping("/register")
     public String productregisterPOST(ProductDTO productDTO, MiddleDTO middleDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
         log.info("product POST register.....");
         productDTO.setMiddleDTO(middleDTO);
@@ -50,11 +50,11 @@ public class ProductController {
 
         int pno = productService.productregister(productDTO);
         redirectAttributes.addFlashAttribute("result", pno);
-        return "redirect:/support/productList";
+        return "redirect:/product/list";
     }
 
     //품목 상세조회
-    @GetMapping({"/productRead", "/productModify"})
+    @GetMapping({"/detail", "/modify"})
     public void productread(int pno, ProductPageRequestDTO productPageRequestDTO, Model model){
         ProductDTO productDTO = productService.productreadOne(pno);
         log.info(productDTO);
@@ -62,23 +62,23 @@ public class ProductController {
     }
 
     //품목 수정
-    @PostMapping("/productModify")
+    @PostMapping("/modify")
     public String productmodify(ProductPageRequestDTO productPageRequestDTO, ProductDTO productDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
         log.info("product modify post........" + productDTO);
         productService.productmodify(productDTO);
         redirectAttributes.addFlashAttribute("result", "modified");
         redirectAttributes.addAttribute("pno", productDTO.getPno());
-        return "redirect:/support/productRead";
+        return "redirect:/product/detail";
 
     }
 
     // 품목 삭제
-    @PostMapping("/productRemove")
+    @PostMapping("/remove")
     public String productremove(int pno, RedirectAttributes redirectAttributes){
         log.info("remove post..." + pno);
         productService.productremove(pno);
         redirectAttributes.addFlashAttribute("result", "removed");
-        return "redirect:/support/productList";
+        return "redirect:/product/list";
     }
 
 
